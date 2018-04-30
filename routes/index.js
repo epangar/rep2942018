1c69061115455c17c39a23587d06457745169967
Tanis Pan <e.pan.gar@gmail.com>
2018-04-30 09:30:24 +0200

    jkj

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router  = express.Router();
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  res.render('index');
+});
+
+/* GET movies page */
+router.get("/movies", (req,res, next) => {
+  res.render("movies")
+
+})
+
+
+module.exports = router;
---
+++ routes/movies.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router  = express.Router();
+const mongoose = require('mongoose');
+
+Movies.find().then((result)=>{
+  
+
+})
+
+module.exports = movies;
